bb211978dd7d3ec531ab7ccc91a4e33395badb75
Kietsiri Chitchainanukul <kietsiri.c@ku.th>
2025-09-18 15:07:15 +0700

    9999

--- assignments/week09/random_quiz-1.py
+++ assignments/week09/random_quiz-1.py
@@ -25,3 +25,28 @@ Example
     Congratulations! You won in 3 attempts!
 
 """
+
+import random
+
+#สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(1, 20)
+
+print("--เกมทายตัวเลข มาเดาใจตอมพิวเตอร์กันเถอะ --")
+print("--ทายเลขจำนวนเต็มตั้งเเต่ 1 - 20 --")
+print("--มีโอกาส 6 ครั้ง --")
+
+for i in range(6):
+
+    #รับค่าการทายเลขจากผู้ใช้
+    print(f"ความพยายามครั้งที่ {i+1}")
+    guess_number = int(input("What if your guess number (1-20)?: "))
+
+# condition ==> if-elif-else
+    if test_random == guess_number:
+        print("ยูเก่งมาก มั่วถูกตั้งเเต่ครั้งเเรกเลย เทพจริงๆ ")
+        break
+    elif guess_number < test_random:
+        print("ผิดจ้า น้อยไปเนอะ")
+
+    elif guess_number > test_random:
+        print("ผิดจ้า มากไปหน่อย") 
--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,24 @@
 import random
 
-def test_random():
-    random_number = random.randint(1, 100)
-    print(random_number)
-    
-test_random()
+#สุ่มเลขระหว่าง 0 - 9
+test_random = random.randint(1, 20)
+
+print("--เกมทายตัวเลข มาเดาใจตอมพิวเตอร์กันเถอะ --")
+print("--ทายเลขจำนวนเต็มตั้งเเต่ 1 - 20 --")
+print("--มีโอกาส 6 ครั้ง --")
+
+for i in range(6):
+
+    #รับค่าการทายเลขจากผู้ใช้
+    print(f"ความพยายามครั้งที่ {i+1}")
+    guess_number = int(input("What if your guess number (1-20)?: "))
+
+# condition ==> if-elif-else
+    if test_random == guess_number:
+        print("ยูเก่งมาก มั่วถูกตั้งเเต่ครั้งเเรกเลย เทพจริงๆ ")
+        break
+    elif guess_number < test_random:
+        print("ผิดจ้า น้อยไปเนอะ")
+
+    elif guess_number > test_random:
+        print("ผิดจ้า มากไปหน่อย") 
